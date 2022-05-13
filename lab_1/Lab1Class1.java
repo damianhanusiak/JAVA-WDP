a512531b8e4a8406367910518b44c2a5e73cfa0c
Damian Hanusiak <hanusiak.damian@gmail.com>
2022-05-13 11:30:38 +0200

    add lab_1 dir

---
+++ lab_1/Lab1Class1.java
@@ -0,0 +1,10 @@
+public class Lab1Class1 {
+	public static void main(String[] Args) {
+		System.out.println("Dzien dobry");
+		System.out.println(dodaj(2, 3));
+	}
+
+	private static int dodaj(int a, int b) {
+		return a + b;
+	}
+}
